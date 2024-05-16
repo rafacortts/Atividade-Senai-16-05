c4b27a93d76976fc5b08f8e706b748eb29b54bdf
Rafael Cortes Soares <NIT0303999@aluno.firjansenaisesi.com.br>
2024-05-16 14:00:07 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.IMG}>
+        <Image
+          source={{ uri: 'https://logos-download.com/wp-content/uploads/2018/02/Clube_de_Regatas_Flamengo_do_Rio_de_Janeiro_RJ_logo.png' }}
+          style={styles.image}
+        />
+        <Image
+          source={{ uri: 'https://logos-download.com/wp-content/uploads/2018/02/Clube_de_Regatas_Flamengo_do_Rio_de_Janeiro_RJ_logo.png' }}
+          style={styles.image}
+        />
+        <Image
+          source={{ uri: 'https://logos-download.com/wp-content/uploads/2018/02/Clube_de_Regatas_Flamengo_do_Rio_de_Janeiro_RJ_logo.png' }}
+          style={styles.image}
+        />
+      </View>
+
+      <View style={styles.img}>
+        <Image
+          source={{ uri: 'https://logos-download.com/wp-content/uploads/2018/02/Clube_de_Regatas_Flamengo_do_Rio_de_Janeiro_RJ_logo.png' }}
+          style={styles.image1}
+        />
+      </View>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <Button
+        title="Login"
+        color={'red'}
+      />
     </View>
   );
 }
@@ -16,5 +52,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  IMG: {
+    flexDirection: 'row',
+    gap: 5
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 5,
+  },
+  image1: {
+    width: 300,
+    height: 300,
+    margin: 0,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    width: '80%',
   },
 });
